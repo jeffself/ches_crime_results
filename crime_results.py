12405a4085d4d84ce9cc750ee1f6820e10462f23
Jeff Self <jeff.self@gmail.com>
2012-03-09 22:33:33 -0500

    Added csv export method

--- crime_results.py
+++ crime_results.py
@@ -1,6 +1,7 @@
 import sys, os
 import urllib, urllib2
 from bs4 import BeautifulSoup
+import csv
 
 def scraper(data):
     """We are getting the crime results data from the
@@ -26,15 +27,28 @@ def scraper(data):
     except:
         print "Invalid URL"
 
+def export_to_csv(data, ofile):
+    f = open(ofile, 'wt')
+    try:
+        fieldnames = ('crime_type', 'description', 'case_number', 'location', 'agency', 'date_reported')
+        writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter='|')
+        headers = {}
+        for r in data:
+            writer.writerow({'crime_type':r['crime_type'], 'description':r['description'], 'case_number':r['case_number'], \
+                             'location':r['location'], 'agency':r['agency'], 'date_reported':r['date_reported']})
+    finally:
+        f.close()
+
 def get_url():
-    return 'http://www.crimemapping.com/DetailedReport.aspx?db=1/01/2012+00:00:00&de=3/07/2012+23:59:00&ccs=AR,AS,BU,DP,DR,DU,FR,HO,VT,RO,SX,TH,VA,VB,WE&xmin=-8498704.744278405&ymin=4397645.076574663&xmax=-8477053.956016656&ymax=4404581.736891533'
+    return 'http://www.crimemapping.com/DetailedReport.aspx?db=1/01/2012+00:00:00&de=3/09/2012+23:59:00&ccs=AR,AS,BU,DP,DR,DU,FR,HO,VT,RO,SX,TH,VA,VB,WE&xmin=-8498704.744278405&ymin=4397645.076574663&xmax=-8477053.956016656&ymax=4404581.736891533'
 
 def main():
+    ofile = sys.argv[1]
     data = []
     scraper(data)
+    export_to_csv(data, ofile)
 
     if len(data) > 0:
-        print data
         print "There are %d crimes listed." % len(data)
 
 if __name__ == '__main__':
